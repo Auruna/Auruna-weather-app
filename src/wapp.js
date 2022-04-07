70f530d2203daf2c20e71e7b3c7140adcbc98f09
Auruna <aurunahaque@gmail.com>
2022-04-07 10:26:38 +0100

    Edited getForecast and displayForecast

--- src/wapp.js
+++ src/wapp.js
@@ -60,7 +60,7 @@ function formatDate(timestamp) {
   return `${day} ${hours} ${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -87,7 +87,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   let apiKey = "f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -173,4 +173,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 searchCity("london");
-displayForecast();
