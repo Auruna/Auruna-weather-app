3dc96ae167b8c41fc741f7a8acf88b61f02d11d7
Auruna <aurunahaque@gmail.com>
2022-04-07 09:47:59 +0100

    Removed the interpolated code from the img src inside function displayForecast

--- src/wapp.js
+++ src/wapp.js
@@ -72,7 +72,7 @@ function displayForecast() {
         <div class="col-2">
           <div class="weather-forecast-date">${day}</div>
           <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/50@2x.png"
           alt=""
           width="42"
         />
